15da6d161fccf31eb716058cccc8e9e27b9beeba
dsebata <dsebaster@gmail.com>
2020-03-27 19:48:27 +0000

    3 files modified

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: 'my-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: [ './demo.component.css' ]
 })
-export class AppComponent  {
+export class DemoComponent  {
   name = 'Angular';
 }
